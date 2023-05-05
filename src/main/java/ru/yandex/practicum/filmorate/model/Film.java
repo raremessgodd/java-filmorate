ef8fe25403315622c372243c1b426be7f8593d28
Tem <tipokruto014@gmail.com>
2023-05-05 17:49:16 +0300

    feat: добавить модели данных

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Film {
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class User {
+    int id;
+    String email;
+    String login;
+    String name;
+    LocalDate birthdayDate;
+}
